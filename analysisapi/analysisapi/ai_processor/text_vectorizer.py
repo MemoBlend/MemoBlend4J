485dceaa76d2db60f270a58ff623e4ad6cafc871
berry8848 <46173232+berry8848@users.noreply.github.com>
2025-04-08 23:22:29 +0900

    クラス化

--- analysisapi/analysisapi/ai_processor/text_vectorizer.py
+++ analysisapi/analysisapi/ai_processor/text_vectorizer.py
@@ -1,38 +1,54 @@
 import chromadb
 
 class TextVectorizer:
-    def __init__(self, persist_directory: str = "./chroma_db"):
-      # DBを保存する場合
-      # chroma_client = chromadb.PersistentClient(path=persist_directory)
-
-      # DBを保存しない場合
-      self.chroma_client = chromadb.Client()
-
+    def __init__(self, 
+                 directory: str = "./chroma_db", 
+                 persist: bool = False, 
+                 user_name: str = "default_user"):
+      """
+      コンストラクタ
+      :param directory: DBの保存先ディレクトリ
+      :param persist: DBを保存するかどうか
+      """
+      if persist:
+        # DBを保存する場合
+        self.chroma_client = chromadb.PersistentClient(path=directory)
+      else:
+        # DBを保存しない場合
+        self.chroma_client = chromadb.Client()
       self.collection = None
+      self.id = 1
+      self.user_name = user_name
 
-    def load_collection(self, name: str = "memo_blend"):        
-      # コレクションを作成または取得
+    def load_collection(self, name: str = "memo_blend"):
+      """
+      コレクションをロードするメソッド。コレクションが存在しない場合は新たに作成する。
+      :param name: コレクションの名前
+      """     
       self.collection = self.chroma_client.create_collection(name=name, get_or_create=True)
 
     def add_text(self, text: str):
-      # ドキュメントを追加
+      """
+      テキストをコレクションに追加するメソッド。
+      :param text: 追加するテキスト
+      """
       self.collection.add(
-        documents=[
-          "今日はハワイに行く予定です",
-          "これはオレンジに関するドキュメントです",
-          "明日は晴れるといいな",
-        ],
-        ids=["id1", "id2", "id3"],
-        metadatas=[
-          {"source": "memo1"},
-          {"source": "memo2"},
-          {"source": "memo3"},
-        ],
+        documents=[text],
+        ids=["id" + str(self.id)],
+        metadatas=[{"source": self.user_name}],
       )
+      self.id += 1
 
-    def serch_text(self, text: str):
+    def query_text(self, target_text: str):
+      """
+      コレクションから target_text に類似するテキストを検索するメソッド。
+      事前にコレクションがロードされていない場合はエラーを返す。
+      :param target_text: 検索するテキスト
+      """
+      if self.collection is None:
+        raise ValueError("コレクションがロードされていません。load_collectionメソッドを呼び出してください。")
       results = self.collection.query(
-        query_texts=["楽しい日記を探したい"],
+        query_texts=[target_text],
         n_results=2
       )
       return results
