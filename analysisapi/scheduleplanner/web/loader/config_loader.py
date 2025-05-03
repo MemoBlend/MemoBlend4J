404fdc6d23094d533ef1f3cf6f47316ccc3287fa
berry8848 <46173232+berry8848@users.noreply.github.com>
2025-05-03 21:30:20 +0900

    レビュ対応

--- analysisapi/scheduleplanner/web/loader/config_loader.py
+++ analysisapi/scheduleplanner/web/loader/config_loader.py
@@ -4,7 +4,7 @@ class ConfigLoader:
   """
   設定ファイル（XML）を読み込むクラス。
   """
-  def __init__(self, file_path: str="../config.xml"):
+  def __init__(self, file_path: str="./web/property/config.xml"):
     """
     ConfigLoader クラスのコンストラクタ。
 
