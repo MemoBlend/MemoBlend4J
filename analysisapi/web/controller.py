292fe2ac141ebee74be45b016343d3d198230a5a
yoshidakenji <jitianxianer018@gmail.com>
2025-05-19 20:00:15 +0900

    コントローラの軽微な修正

--- analysisapi/web/controller.py
+++ analysisapi/web/controller.py
@@ -16,10 +16,14 @@ class Controller:
     def __init__(self):
         self.router = APIRouter()
         self.router.add_api_route(
-            "/user/{user_id}/{diary_id}", self.get_diary_add_db, methods=["GET"]
+            path="/diary/{user_id}/{diary_id}",
+            endpoint=self.get_diary_add_db,
+            methods=["GET"],
         )
         self.router.add_api_route(
-            "/analysis/scheduler/{user_id}", self.get_schedule, methods=["GET"]
+            path="/schedule/{user_id}",
+            endpoint=self.get_schedule_suggestion,
+            methods=["GET"],
         )
         self.common_application_service = CommonApplicationService()
         self.client_application_service = ClientApplicationService()
@@ -40,8 +44,7 @@ class Controller:
             RequestException: 通信中にエラーが発生した場合。
             Exception: その他の予期しないエラー。
         """
-        diary_get_url = Constants.DIARY_GET_URL
-        url = f"{diary_get_url}/{diary_id}"
+        url = f"{Constants.DIARY_GET_URL}/{diary_id}"
 
         try:
             with httpx.Client() as client:
@@ -71,12 +74,12 @@ class Controller:
 
         return {"message": "日記が正常にDBに追加されました"}
 
-    def get_schedule(self, user_id: int) -> str:
+    def get_schedule_suggestion(self, user_id: int) -> str:
         """
-        過去の日記を分析して、明日の予定を提案します。
+        過去の日記を分析して、明日の予定の提案を取得します。
 
         Returns:
-            str: 明日の予定。
+            str: 明日の予定の提案。
         """
         location = {"latitude": 35.7001076, "longitude": 139.9855455}
 
