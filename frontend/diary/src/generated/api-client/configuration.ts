28ff4b255d4e3e9632910921937d65a121ee6912
yoshidakenji <jitianxianer018@gmail.com>
2025-05-21 21:41:04 +0900

    foamattingを実施

--- frontend/diary/src/generated/api-client/configuration.ts
+++ frontend/diary/src/generated/api-client/configuration.ts
@@ -14,17 +14,17 @@
 
 export interface ConfigurationParameters {
   apiKey?:
-  | string
-  | Promise<string>
-  | ((name: string) => string)
-  | ((name: string) => Promise<string>);
+    | string
+    | Promise<string>
+    | ((name: string) => string)
+    | ((name: string) => Promise<string>);
   username?: string;
   password?: string;
   accessToken?:
-  | string
-  | Promise<string>
-  | ((name?: string, scopes?: string[]) => string)
-  | ((name?: string, scopes?: string[]) => Promise<string>);
+    | string
+    | Promise<string>
+    | ((name?: string, scopes?: string[]) => string)
+    | ((name?: string, scopes?: string[]) => Promise<string>);
   basePath?: string;
   serverIndex?: number;
   baseOptions?: any;
