f85d674d43de67c9cc4f21bdf9254dfd86e8979d
yoshidakenji <jitianxianer018@gmail.com>
2025-05-24 13:38:37 +0900

    例外ハンドラ登録のタイポを修正

--- analysisapi/app.py
+++ analysisapi/app.py
@@ -1,15 +1,20 @@
 """Analysis API アプリケーションを起動するモジュールです。"""
 
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 import uvicorn
 from presentation.controller import Controller
 from presentation.exception_handlers import ExceptionHandlers
 import settings
+from systemcommon.business_exception import BusinessException
+from systemcommon.system_exception import SystemException
 
 
 app = FastAPI()
 app.include_router(Controller().router, prefix="/api", tags=["analysisapi"])
-app.add_exception_handler(HTTPException, ExceptionHandlers.system_exception_handler)
+app.add_exception_handler(
+    BusinessException, ExceptionHandlers.business_exception_handler
+)
+app.add_exception_handler(SystemException, ExceptionHandlers.system_exception_handler)
 app.add_exception_handler(Exception, ExceptionHandlers.global_exception_handler)
 
 
