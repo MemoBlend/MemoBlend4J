83a1a6a8133f76a97e2ae5a2c86624038bd4bb39
yoshidakenji <jitianxianer018@gmail.com>
2025-05-24 13:31:48 +0900

    業務例外のハンドリングを追加

--- analysisapi/presentation/exception_handlers.py
+++ analysisapi/presentation/exception_handlers.py
@@ -16,6 +16,29 @@ class ExceptionHandlers:
     logger = LoggerConfig.get_logger(name=__name__, level=logging.INFO)
 
     @staticmethod
+    async def business_exception_handler(_request: Request, exception: SystemException):
+        """
+        ビジネス例外を処理するための例外ハンドラーです。
+
+        Args:
+            request (Request): リクエストオブジェクト。
+            exception (SystemException): 発生したビジネス例外。
+
+        Returns:
+            JSONResponse: エラーレスポンス。
+        """
+
+        ExceptionHandlers.logger.error(
+            "システムエラーが発生しました: %s\n%s", exception, traceback.format_exc()
+        )
+        return JSONResponse(
+            status_code=500,
+            content={
+                "message": "システムエラーが発生しました",
+            },
+        )
+
+    @staticmethod
     async def system_exception_handler(_request: Request, exception: SystemException):
         """
         システム例外を処理するための例外ハンドラーです。
