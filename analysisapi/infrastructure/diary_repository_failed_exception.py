57d3dfaadbd51656f7cb938c0d67dbf9c3019335
berry8848 <formic3776@gmail.com>
2025-07-05 13:07:19 +0900

    DiaryRepositoryFailedException のエラーメッセージを定数を使用して更新

--- analysisapi/infrastructure/diary_repository_failed_exception.py
+++ analysisapi/infrastructure/diary_repository_failed_exception.py
@@ -2,6 +2,7 @@
 データベースへ文章を追加することに失敗した場合の例外クラスです。
 """
 
+from analysisapi.infrastructure.infrastructure_constants import InfrastructureConstants
 from systemcommon.business_exception import BusinessException
 
 
@@ -13,5 +14,5 @@ class DiaryRepositoryFailedException(BusinessException):
     """
 
     def __init__(self):
-        message = f"CHUNK_OVERLAP は CHUNK_SIZE 未満を指定してください"
+        message = f"{InfrastructureConstants.CHUNK_OVERLAP} は {InfrastructureConstants.CHUNK_SIZE} 未満を指定してください"
         super().__init__(message)
